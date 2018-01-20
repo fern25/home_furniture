62feacb6d3ee50f060364d6356e2ec9b3ce04126
waput90 <tubianojames@gmail.com>
2018-01-21 02:41:09 +0800

    new products in the local resources

---
+++ home.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.min.css">
+	<style type="text/css">
+	img{
+		border-radius: 5px;
+	}
+</style>
+</head>
+<body style="background: url('bg2.jpg');background-size: cover;">
+	<div class="container">
+		<div class="navbar navbar-inverse">
+			<div class="navbar-header">
+				<a href="" class="navbar-brand">Home Furniture</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li>
+					<a href="">Items</a>
+				</li>
+				<li class="active">
+					<a href="">Services</a>
+				</li>
+				<li>
+					<a href="">Contact us</a>
+				</li>
+				<li>
+					<a href="">About Us</a>
+				</li>
+			</ul>
+		</div>
+		<br><br><br><br><br>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h4>This is a heading!</h4>
+			</div>
+			<div class="panel-body" style="background: #225;">
+				<div class="col-sm-12">
+					<div class="col-sm-3">
+						<a href="" class="btn btn-success">
+							<img src="1.jpg" width="100" height="100">
+							<br><br>
+							<label>Silver white Antique Watch</label>
+							<p style="color: orange">₱12,299.75</p>
+						</a>
+					</div>
+					<div class="col-sm-3">
+						<a href="" class="btn btn-primary">
+							<img src="1.jpg" width="100" height="100">
+							<br><br>
+							<label>Silver white Antique Watch</label>
+							<p style="color: orange">₱12,299.75</p>
+						</a>
+					</div>
+					<div class="col-sm-3">
+						<a href="" class="btn btn-danger">
+							<img src="1.jpg" width="100" height="100">
+							<br><br>
+							<label>Silver white Antique Watch</label>
+							<p style="color: orange">₱12,299.75</p>
+						</a>
+					</div>
+					<div class="col-sm-3">
+						<a href="" class="btn btn-warning">
+							<img src="1.jpg" width="100" height="100">
+							<br><br>
+							<label>Silver white Antique Watch</label>
+							<p style="color: orange">₱12,299.75</p>
+						</a>
+					</div>
+					<div class="col-sm-12"><br></div>
+					<div class="col-sm-3">
+						<a href="" class="btn btn-default">
+							<img src="1.jpg" width="100" height="100">
+							<br><br>
+							<label>Silver white Antique Watch</label>
+							<p style="color: orange">₱12,299.75</p>
+						</a>
+					</div>
+					<div class="col-sm-3">
+						<a href="" class="btn btn-default">
+							<img src="1.jpg" width="100" height="100">
+							<br><br>
+							<label>Silver white Antique Watch</label>
+							<p style="color: orange">₱12,299.75</p>
+						</a>
+					</div>
+					<div class="col-sm-3">
+						<a href="" class="btn btn-default">
+							<img src="1.jpg" width="100" height="100">
+							<br><br>
+							<label>Silver white Antique Watch</label>
+							<p style="color: orange">₱12,299.75</p>
+						</a>
+					</div>
+					<div class="col-sm-3">
+						<a href="" class="btn btn-default">
+							<img src="1.jpg" width="100" height="100">
+							<br><br>
+							<label>Silver white Antique Watch</label>
+							<p style="color: orange">₱12,299.75</p>
+						</a>
+					</div>
+					<div class="col-sm-2">
+						<a href="" class="btn btn-default">
+							<img src="1.jpg" width="100" height="100">
+							<br><br>
+							<label>Silver white Antique Watch</label>
+							<p style="color: orange">₱12,299.75</p>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="panel-footer">
+				<h4>This is a footer!</h4>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
